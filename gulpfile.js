41ef5fc4ae1075fbee903b9124a9ad98220c4ba7
WARREN GORDON <warrengordon@WARRENs-MacBook-Pro.local>
2017-04-26 14:47:59 -0400

    add postcss autoprefixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss    = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
   return gulp.src('./src/index.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe( postcss([ require('autoprefixer') ]) )
+    .pipe( sourcemaps.write('.') )
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('default', function () {
