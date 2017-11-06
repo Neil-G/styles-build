b3d24f6f28ca68129107349e774a850a76d186be
WARREN GORDON <warrengordon@WARRENs-MacBook-Pro.local>
2017-11-06 11:03:35 -0500

    switch to LESS

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss    = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var path = require('path');
 
 gulp.task('build-sass', function () {
-  return gulp.src('./src/index.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./src/index.less')
+    .pipe(less({
+    	paths: [],
+    }).on('error', sass.logError))
     .pipe( postcss([ require('autoprefixer') ]) )
     .pipe( sourcemaps.write('.') )
     .pipe(gulp.dest('./build'));
 });
 
 gulp.task('default', function () {
-  gulp.watch('./src/*.scss', ['build-sass']);
+  gulp.watch('./src/**/*.less', ['build-sass']);
 });
